a78e3465c50d50ccc19a60d6ae18c231dad7df37
gautambhatt1996 <gautambhatt1996@gmail.com>
2020-12-06 21:33:37 +0530

    Updated app.py with SparkContext

--- Examples/app.py
+++ Examples/app.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 logFile="file:////Users/gautam/Downloads/Spark/spark-3.0.0-preview2-bin-hadoop3.2/README.md"
-#sc = SparkContext("local", "first app")
+sc = SparkContext("local", "app")
 logData = sc.textFile(logFile).cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()
